675484a0f3a760b4a65d006fd78901203735bf61
Jagahkiin2014 <pacman1225@gmail.com>
2016-10-19 16:23:32 -0500

    Initial

---
+++ src/com/github/jagahkiin2014/Jagglessentials/Jagglessentials.java
@@ -0,0 +1,16 @@
+package com.github.jagahkiin2014.Jagglessentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Jagglessentials extends JavaPlugin {
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	@Override
+	public void onEnable() {
+		
+	}
+}
