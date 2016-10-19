b099745ebb736c44309126d9e713909a84f15f57
Jagahkiin2014 <pacman1225@gmail.com>
2016-10-19 16:46:49 -0500

    Essential functions

--- src/com/github/jagahkiin2014/Jagglessentials/Jagglessentials.java
+++ src/com/github/jagahkiin2014/Jagglessentials/Jagglessentials.java
@@ -1,8 +1,19 @@
 package com.github.jagahkiin2014.Jagglessentials;
 
+import java.io.File;
+import java.util.List;
+
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.github.jagahkiin2014.Jagglessentials.Utils.Log;
+
 public class Jagglessentials extends JavaPlugin {
+	public String version;
+	public List<String> authors;
+	
+	public static File SettingsDir;
+	public static File SettingsFile;
 	
 	@Override
 	public void onDisable() {
@@ -11,6 +22,20 @@ public class Jagglessentials extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
+		PluginDescriptionFile pdFile = this.getDescription();
+		version = pdFile.getVersion();
+		authors = pdFile.getAuthors();
+		
+		Log.LogMessage("[JE] Starting Jagglessentials v" + version + "...", getServer().getConsoleSender());
+		
+		if(!getDataFolder().exists()) {
+			getDataFolder().mkdir();
+		}
+		SettingsDir = new File(getDataFolder() + "/Settings");
+		SettingsFile = new File(SettingsDir, "settings.yml");
+		if(!SettingsFile.exists()) {
+			saveResource("Settings/settings.yml", false);
+		}
 		
 	}
 }
