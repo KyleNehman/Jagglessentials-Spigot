46052c76623ae7cf8b7cc3d6f88eb28d0486e51a
Jagahkiin2014 <pacman1225@gmail.com>
2016-10-21 09:31:36 -0500

    More work on offline player

--- src/com/github/jagahkiin2014/Jagglessentials/Commands/Seen.java
+++ src/com/github/jagahkiin2014/Jagglessentials/Commands/Seen.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
+import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -47,76 +48,10 @@ public class Seen implements CommandExecutor {
 					
 					if(sender.hasPermission("je.seen.normal")) {
 						
-						if(!target.isOnline()) {
-							seenTop(sender);
-							
-							try {
-								uuidFile.load(Jagglessentials.uuid);
-								String offUuid = uuidFile.getString(target.getName());
-								String aliases = uuidFile.getStringList("known-aliases").toString().replace("[", "").replace("]", "");
-								
-								JECommand.msg(sender, "&6Status: &cOffline");
-								JECommand.msg(sender, "&6UUID:&e " + offUuid);
-								JECommand.msg(sender, "&6Known Aliases: &e" + aliases);
-								JECommand.msg(sender, "&6Last Online: " + target.getLastPlayed());
-							
-							} catch(IOException | InvalidConfigurationException e1) {
-								e1.printStackTrace();
-							}
-							
-							seenBot(sender);
-							
-						} else {
-							seenTop(sender);
-							
-							JECommand.msg(sender, "&6Status: &aOnline");
-							JECommand.msg(sender, "&6UUID:&e " + uuid);
-							JECommand.msg(sender, "&6Known Aliases: &e" + getAliases(sender, target));
-							
-							seenBot(sender);
-						}
 						
 					} else if(sender.hasPermission("je.seen.staff")) {
 						
-						if(!target.isOnline()) {
-							try {
-								uuidFile.load(Jagglessentials.uuid);
-								String offUuid = uuidFile.getString(target.getName());
-								String aliases = uuidFile.getStringList("known-aliases").toString().replace("[", "").replace("]", "");
-								
-								seenTop(sender);
-								
-								JECommand.msg(sender, "&6Status: &cOffline");
-								JECommand.msg(sender, "&6UUID:&e " + offUuid);
-								JECommand.msg(sender, "&6Known Aliases: &e" + aliases);
-								JECommand.msg(sender, "&6Last Online:&e " + target.getLastPlayed());
-								if(sender.hasPermission("je.seen.ip")) {
-									userInfo.load(userFile);
-									JECommand.msg(sender, "&6Last Known IP:&e " + userInfo.get("last-seen.ip"));
-								}
-								
-								seenBot(sender);
-							} catch (IOException | InvalidConfigurationException e) {
-								e.printStackTrace();
-							}
-						} else {
-							seenTop(sender);
-							
-							JECommand.msg(sender, "&6Status: &aOnline");
-							JECommand.msg(sender, "&6UUID:&e " + uuid);
-							JECommand.msg(sender, "&6Known Aliases: &e" + getAliases(sender, target));
-							JECommand.msg(sender, "&6IP Address:&e " + target.getServer().getIp());
-							JECommand.msg(sender, "");
-							JECommand.msg(sender, "&6+----&bHistory&6----+");
-							JECommand.msg(sender, "&6Kicks:&e " + (userInfo.getStringList("history.kicks").size() - 1));
-							JECommand.msg(sender, "&6Tempbans:&e " + (userInfo.getStringList("history.tempbans").size() - 1));
-							JECommand.msg(sender, "&6Bans:&e " + (userInfo.getStringList("history.bans").size() - 1));
-							JECommand.msg(sender, "&6Unbans:&e " + (userInfo.getStringList("history.unbans").size() - 1));
-							JECommand.msg(sender, "&8Use '&7/history <player>&8' for a detailed synopsis.");
-							
-							seenBot(sender);
-							
-						}
+						
 					}
 				}
 			} else {
@@ -129,6 +64,90 @@ public class Seen implements CommandExecutor {
 	
 	private void seenTop(CommandSender sender) {
 		sender.sendMessage(Log.ColorMessage("&6+----------&bUser&6--&bInfo&6----------+"));
+	}
+	
+	private void seenNorm(CommandSender sender, String[] args) {
+		Player target = Bukkit.getServer().getPlayer(args[0]);
+		if(!target.isOnline()) {
+			seenTop(sender);
+			
+			File uuid = new File(plugin.getDataFolder(), "uuid.yml");
+			YamlConfiguration uuidFile = new YamlConfiguration();
+			try {
+				uuidFile.load(Jagglessentials.uuid);
+				String offUuid = uuidFile.getString(target.getName());
+				String aliases = uuidFile.getStringList("known-aliases").toString().replace("[", "").replace("]", "");
+				
+				JECommand.msg(sender, "&6Status: &cOffline");
+				JECommand.msg(sender, "&6UUID:&e " + offUuid);
+				JECommand.msg(sender, "&6Known Aliases: &e" + aliases);
+				JECommand.msg(sender, "&6Last Online: " + target.getLastPlayed());
+			
+			} catch(IOException | InvalidConfigurationException e1) {
+				e1.printStackTrace();
+			}
+			
+			seenBot(sender);
+			
+		} else {
+			seenTop(sender);
+			
+			JECommand.msg(sender, "&6Status: &aOnline");
+			JECommand.msg(sender, "&6UUID:&e " + uuid);
+			JECommand.msg(sender, "&6Known Aliases: &e" + getAliases(sender, target));
+			
+			seenBot(sender);
+		}
+	}
+	
+	private void seenStaff(CommandSender sender, String[] args) {
+		Player target = Bukkit.getServer().getPlayer(args[0]);
+		if(!target.isOnline()) {
+			try {
+				File uuid = new File(plugin.getDataFolder(), "uuid.yml");
+				YamlConfiguration uuidFile = new YamlConfiguration();
+				
+				uuidFile.load(Jagglessentials.uuid);
+				String offUuid = uuidFile.getString(args[0]);
+				
+				File userFile = new File(Jagglessentials.UserDir + File.separator, offUuid + ".yml");
+				YamlConfiguration userInfo = new YamlConfiguration();
+				
+				String aliases = userInfo.getStringList("known-aliases").toString().replace("[", "").replace("]", "");
+				
+				seenTop(sender);
+				
+				JECommand.msg(sender, "&6Status: &cOffline");
+				JECommand.msg(sender, "&6UUID:&e " + offUuid);
+				JECommand.msg(sender, "&6Known Aliases: &e" + aliases);
+				JECommand.msg(sender, "&6Last Online:&e " + target.getLastPlayed());
+				if(sender.hasPermission("je.seen.ip")) {
+					userInfo.load(userFile);
+					JECommand.msg(sender, "&6Last Known IP:&e " + userInfo.get("last-seen.ip"));
+				}
+				
+				seenBot(sender);
+			} catch (IOException | InvalidConfigurationException e) {
+				e.printStackTrace();
+			}
+		} else {
+			seenTop(sender);
+			
+			JECommand.msg(sender, "&6Status: &aOnline");
+			JECommand.msg(sender, "&6UUID:&e " + uuid);
+			JECommand.msg(sender, "&6Known Aliases: &e" + getAliases(sender, target));
+			JECommand.msg(sender, "&6IP Address:&e " + target.getServer().getIp());
+			JECommand.msg(sender, "");
+			JECommand.msg(sender, "&6+----&bHistory&6----+");
+			JECommand.msg(sender, "&6Kicks:&e " + (userInfo.getStringList("history.kicks").size() - 1));
+			JECommand.msg(sender, "&6Tempbans:&e " + (userInfo.getStringList("history.tempbans").size() - 1));
+			JECommand.msg(sender, "&6Bans:&e " + (userInfo.getStringList("history.bans").size() - 1));
+			JECommand.msg(sender, "&6Unbans:&e " + (userInfo.getStringList("history.unbans").size() - 1));
+			JECommand.msg(sender, "&8Use '&7/history <player>&8' for a detailed synopsis.");
+			
+			seenBot(sender);
+			
+		}
 	}
 	
 	private void seenBot(CommandSender sender) {
